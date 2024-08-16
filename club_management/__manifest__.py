182d39fdcd32be1a641bbf68647af515f9a856b4
mahmoudhanyy <mahmoud.hanyy007@gmail.com>
2024-08-16 19:10:55 +0300

    [ADD] club management module setup

---
+++ club_management/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Club Management',
+    'version': '17.0.0.1',
+    'category': 'Calendar',
+    'summary': '',
+    'depends': [
+        'base',
+        'contacts',
+        'calendar',
+    ],
+    'data': [
+        'views/club_management_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
